f58ba3151bd99840d7909632dbb00ac5c710956f
danmacode <danmacode1@gmail.com>
2023-03-06 23:36:39 +0100

    create insert_data.sh & pg_dump

--- 02-world-cup/insert_data.sh
+++ 02-world-cup/insert_data.sh
@@ -9,3 +9,54 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database
+
+# remove all rows from set of tables
+echo "$($PSQL "TRUNCATE TABLE games, teams")"
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
+  # for all except the header "year"
+  if [[ $YEAR != "year" ]]; then
+    ##
+    # inserting team_id of winner/opponent into teams TABLE
+    ##
+
+    # team_id of winner = SELECT team_id FROM teams WHERE name = WINNER(aka )
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    # if team_id not found
+    if [[ -z $WINNER_ID ]]; then
+      # insert the name of the winner
+      INSERT_WIN_NAME_RET=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      # succesful insert
+      if [[ $INSERT_WIN_NAME_RET = "INSERT 0 1" ]]; then
+        printf "teams - insert winner: $WINNER\n"
+      fi
+      # team_id of winner = SELECT team_id FROM teams WHERE name=WINNER
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+
+    # get opponent's team_id
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    # if team_id not found
+    if [[ -z $OPPONENT_ID ]]; then
+      # insert the name of the opponent
+      INSERT_OP_NAME_RET=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      # succesful insert
+      if [[ $INSERT_OP_NAME_RET = "INSERT 0 1" ]]; then
+        echo "teams - insert opponent: $OPPONENT\n"
+      fi
+      # get the new opponent team_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    ##
+    # inserting data into games TABLE
+    ##
+    INSERT_RET=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
+    VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    # if successful insert then print all row
+    if [[ $INSERT_RET = "INSERT 0 1" ]]; then
+      printf "games - insert $YEAR, '$ROUND', [$WINNER_ID vs $OPPONENT_ID]: $WINNER_GOALS - $OPPONENT_GOALS\n"
+    fi
+
+  fi
+done
--- 02-world-cup/worldcup.sql
+++ 02-world-cup/worldcup.sql
@@ -134,26 +134,82 @@ ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.t
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.games VALUES (1, 'Final', 2018, 1, 2, 4, 2);
+INSERT INTO public.games VALUES (2, 'Third Place', 2018, 3, 4, 2, 0);
+INSERT INTO public.games VALUES (3, 'Semi-Final', 2018, 2, 4, 2, 1);
+INSERT INTO public.games VALUES (4, 'Semi-Final', 2018, 1, 3, 1, 0);
+INSERT INTO public.games VALUES (5, 'Quarter-Final', 2018, 2, 5, 3, 2);
+INSERT INTO public.games VALUES (6, 'Quarter-Final', 2018, 4, 6, 2, 0);
+INSERT INTO public.games VALUES (7, 'Quarter-Final', 2018, 3, 7, 2, 1);
+INSERT INTO public.games VALUES (8, 'Quarter-Final', 2018, 1, 8, 2, 0);
+INSERT INTO public.games VALUES (9, 'Eighth-Final', 2018, 4, 9, 2, 1);
+INSERT INTO public.games VALUES (10, 'Eighth-Final', 2018, 6, 10, 1, 0);
+INSERT INTO public.games VALUES (11, 'Eighth-Final', 2018, 3, 11, 3, 2);
+INSERT INTO public.games VALUES (12, 'Eighth-Final', 2018, 7, 12, 2, 0);
+INSERT INTO public.games VALUES (13, 'Eighth-Final', 2018, 2, 13, 2, 1);
+INSERT INTO public.games VALUES (14, 'Eighth-Final', 2018, 5, 14, 2, 1);
+INSERT INTO public.games VALUES (15, 'Eighth-Final', 2018, 8, 15, 2, 1);
+INSERT INTO public.games VALUES (16, 'Eighth-Final', 2018, 1, 16, 4, 3);
+INSERT INTO public.games VALUES (17, 'Final', 2014, 17, 16, 1, 0);
+INSERT INTO public.games VALUES (18, 'Third Place', 2014, 18, 7, 3, 0);
+INSERT INTO public.games VALUES (19, 'Semi-Final', 2014, 16, 18, 1, 0);
+INSERT INTO public.games VALUES (20, 'Semi-Final', 2014, 17, 7, 7, 1);
+INSERT INTO public.games VALUES (21, 'Quarter-Final', 2014, 18, 19, 1, 0);
+INSERT INTO public.games VALUES (22, 'Quarter-Final', 2014, 16, 3, 1, 0);
+INSERT INTO public.games VALUES (23, 'Quarter-Final', 2014, 7, 9, 2, 1);
+INSERT INTO public.games VALUES (24, 'Quarter-Final', 2014, 17, 1, 1, 0);
+INSERT INTO public.games VALUES (25, 'Eighth-Final', 2014, 7, 20, 2, 1);
+INSERT INTO public.games VALUES (26, 'Eighth-Final', 2014, 9, 8, 2, 0);
+INSERT INTO public.games VALUES (27, 'Eighth-Final', 2014, 1, 21, 2, 0);
+INSERT INTO public.games VALUES (28, 'Eighth-Final', 2014, 17, 22, 2, 1);
+INSERT INTO public.games VALUES (29, 'Eighth-Final', 2014, 18, 12, 2, 1);
+INSERT INTO public.games VALUES (30, 'Eighth-Final', 2014, 19, 23, 2, 1);
+INSERT INTO public.games VALUES (31, 'Eighth-Final', 2014, 16, 10, 1, 0);
+INSERT INTO public.games VALUES (32, 'Eighth-Final', 2014, 3, 24, 2, 1);
 
 
 --
 -- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.teams VALUES (1, 'France');
+INSERT INTO public.teams VALUES (2, 'Croatia');
+INSERT INTO public.teams VALUES (3, 'Belgium');
+INSERT INTO public.teams VALUES (4, 'England');
+INSERT INTO public.teams VALUES (5, 'Russia');
+INSERT INTO public.teams VALUES (6, 'Sweden');
+INSERT INTO public.teams VALUES (7, 'Brazil');
+INSERT INTO public.teams VALUES (8, 'Uruguay');
+INSERT INTO public.teams VALUES (9, 'Colombia');
+INSERT INTO public.teams VALUES (10, 'Switzerland');
+INSERT INTO public.teams VALUES (11, 'Japan');
+INSERT INTO public.teams VALUES (12, 'Mexico');
+INSERT INTO public.teams VALUES (13, 'Denmark');
+INSERT INTO public.teams VALUES (14, 'Spain');
+INSERT INTO public.teams VALUES (15, 'Portugal');
+INSERT INTO public.teams VALUES (16, 'Argentina');
+INSERT INTO public.teams VALUES (17, 'Germany');
+INSERT INTO public.teams VALUES (18, 'Netherlands');
+INSERT INTO public.teams VALUES (19, 'Costa Rica');
+INSERT INTO public.teams VALUES (20, 'Chile');
+INSERT INTO public.teams VALUES (21, 'Nigeria');
+INSERT INTO public.teams VALUES (22, 'Algeria');
+INSERT INTO public.teams VALUES (23, 'Greece');
+INSERT INTO public.teams VALUES (24, 'United States');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 1, false);
+SELECT pg_catalog.setval('public.games_game_id_seq', 32, true);
 
 
 --
 -- Name: teams_team_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.teams_team_id_seq', 1, false);
+SELECT pg_catalog.setval('public.teams_team_id_seq', 24, true);
 
 
 --
