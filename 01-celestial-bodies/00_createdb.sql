2fbaa6f72ca0d27d153d49d273d9e2603df1470f
danmacode <danmacode1@gmail.com>
2023-04-04 13:04:14 +0200

    add: galaxy TABLE creation

--- 01-celestial-bodies/00_createdb.sql
+++ 01-celestial-bodies/00_createdb.sql
@@ -1,52 +1,50 @@
+--WARNING! ERRORS ENCOUNTERED DURING SQL PARSING!
 -- -*-mode: sql; sql-product: postgres;-*- vim:ft=postgresql
 --
--- github needs the vim modeline to categorize this as a PLpgSQL file 
+-- github needs the vim modeline to categorize this as a PLpgSQL file
 CREATE DATABASE universe;
 
 -- connect to the database with \c universe
 -- then create all 4 tables with table_name_id naming convention
 -- all of them with a name column, and 5 columns
-CREATE TABLE
-    IF NOT EXISTS galaxy (
-        galaxy_id SERIAL PRIMARY KEY NOT NULL,
-        name VARCHAR(30) NOT NULL UNIQUE,
-        speed INT,
-        description TEXT
-    );
+-- each table with 3+ rows
+CREATE TABLE IF NOT EXISTS galaxy (
+    galaxy_id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    speed NUMERIC NOT NULL DEFAULT (1),
+    description TEXT
+);
 
-CREATE TABLE
-    IF NOT EXISTS star (
-        star_id SERIAL PRIMARY KEY,
-        name VARCHAR(30) NOT NULL UNIQUE,
-        galaxy_id INT REFERENCES galaxy (galaxy_id),
-        description TEXT
-    );
+CREATE TABLE IF NOT EXISTS star (
+    star_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    galaxy_id INT REFERENCES galaxy (galaxy_id),
+    description TEXT
+);
 
-CREATE TABLE
-    IF NOT EXISTS planet (
-        planet_id SERIAL PRIMARY KEY,
-        name VARCHAR(30) NOT NULL UNIQUE,
-        dwarf BOOLEAN NOT NULL DEFAULT FALSE, -- name == pluto 
-        moons INT NOT NULL DEFAULT (0),
-        star_id INT REFERENCES star (star_id),
-        description TEXT
-    );
+CREATE TABLE IF NOT EXISTS planet (
+    planet_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    -- e.g. pluto
+    dwarf BOOLEAN NOT NULL DEFAULT FALSE,
+    moons INT NOT NULL DEFAULT (0),
+    star_id INT REFERENCES star (star_id),
+    description TEXT
+);
 
--- 🤓 actually, these are satellites, not "moons" 
-CREATE TABLE
-    IF NOT EXISTS moon (
-        moon_id SERIAL PRIMARY KEY,
-        name VARCHAR(30) NOT NULL UNIQUE,
-        lone BOOLEAN NOT NULL DEFAULT TRUE,
-        radius NUMERIC,
-        partners INT NOT NULL DEFAULT (0),
-        planet_id INT REFERENCES planet (planet_id),
-        description TEXT
-    );
+-- 🤓 actually, these are satellites, not "moons"
+CREATE TABLE IF NOT EXISTS moon (
+    moon_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    lone BOOLEAN NOT NULL DEFAULT TRUE,
+    radius NUMERIC,
+    partners INT NOT NULL DEFAULT (0),
+    planet_id INT REFERENCES planet (planet_id),
+    description TEXT
+);
 
 -- we need 5 tables in total to pass the test
-CREATE TABLE
-    IF NOT EXISTS filler (
-        filler_id SERIAL PRIMARY KEY,
-        name VARCHAR(30) NOT NULL UNIQUE,
-    );
+CREATE TABLE IF NOT EXISTS filler (
+    filler_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+);
--- 01-celestial-bodies/universe.sql
+++ 01-celestial-bodies/universe.sql
@@ -1,45 +1,100 @@
 -- -*-mode: sql; sql-product: postgres;-*- vim:ft=postgresql
 --
--- PostgreSQL database dump - initial database dump after creation
+-- PostgreSQL database dump - db dump after galaxy table creation
 
 -- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
 -- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
-
 SET statement_timeout = 0;
-SET lock_timeout = 0;
+SET LOCK_TIMEOUT = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
+SET standard_conforming_strings = ON;
+
 SELECT pg_catalog.set_config('search_path', '', false);
+
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
-SET row_security = off;
+SET row_security = OFF;
 
 DROP DATABASE universe;
+
 --
 -- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
 --
-
-CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-
+CREATE DATABASE universe
+    WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
 
 ALTER DATABASE universe OWNER TO freecodecamp;
 
 \connect universe
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
+SET LOCK_TIMEOUT = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
+SET standard_conforming_strings = ON;
+
 SELECT pg_catalog.set_config('search_path', '', false);
+
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
-SET row_security = off;
+SET row_security = OFF;
+SET default_tablespace = '';
+SET default_table_access_method = heap;
 
+--
+-- Name: galaxy; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+CREATE TABLE PUBLIC.galaxy (
+    galaxy_id INTEGER NOT NULL,
+    name CHARACTER VARYING(30) NOT NULL,
+    speed NUMERIC DEFAULT 1 NOT NULL,
+    description TEXT
+    );
+
+ALTER TABLE PUBLIC.galaxy OWNER TO freecodecamp;
+
+--
+-- Name: galaxy_galaxy_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+CREATE SEQUENCE PUBLIC.galaxy_galaxy_id_seq AS INTEGER START
+    WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+
+ALTER TABLE PUBLIC.galaxy_galaxy_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: galaxy_galaxy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+ALTER SEQUENCE PUBLIC.galaxy_galaxy_id_seq OWNED BY PUBLIC.galaxy.galaxy_id;
+
+--
+-- Name: galaxy galaxy_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+ALTER TABLE ONLY PUBLIC.galaxy
+
+ALTER COLUMN galaxy_id
+
+SET DEFAULT nextval('public.galaxy_galaxy_id_seq'::regclass);
+
+--
+-- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+--
+-- Name: galaxy_galaxy_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 1, false);
+
+--
+-- Name: galaxy galaxy_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+ALTER TABLE ONLY PUBLIC.galaxy ADD CONSTRAINT galaxy_name_key UNIQUE (name);
+
+--
+-- Name: galaxy galaxy_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+ALTER TABLE ONLY PUBLIC.galaxy ADD CONSTRAINT galaxy_pkey PRIMARY KEY (galaxy_id);
 --
 -- PostgreSQL database dump complete
 --
-
